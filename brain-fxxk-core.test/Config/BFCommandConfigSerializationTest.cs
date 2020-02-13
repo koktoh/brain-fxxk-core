482e58d7902698b061fad575b4afedb1ba3777a2
koktoh <koktoh96@gmail.com>
2020-02-13 21:53:25 +0900

    fix typo

--- brain-fxxk-core.test/Config/BFCommandConfigSerializationTest.cs
+++ brain-fxxk-core.test/Config/BFCommandConfigSerializationTest.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO;
 using BFCore.Command;
 using BFCore.Config;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,12 +8,21 @@ namespace brain_fxxk_core.test.Config
     [TestClass]
     public class BFCommandConfigSerializationTest
     {
+        private readonly string _destPath = @"TestData\Result.json";
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            if (File.Exists(this._destPath))
+            {
+                File.Delete(this._destPath);
+            }
+        }
+
         [TestMethod]
         public void SerializeCorrectlyTest()
         {
-            var jsonPath = @"TestData\BFCommandConfigJsons\Normal.json";
-
-            var expected = new BFCommandConfig
+            var src = new BFCommandConfig
             {
                 Increment = new BFCommand("inc", BFCommandType.Increment),
                 Decrement = new BFCommand("dec", BFCommandType.Decrement),
@@ -29,33 +36,11 @@ namespace brain_fxxk_core.test.Config
                 EndComment = new BFCommand("endC", BFCommandType.EndComment)
             };
 
-            var result = ConfigManager.Import<BFCommandConfig>(jsonPath);
+            var expectedFile = @"TestData\BFCommandConfigJsons\Expected.json";
 
-            result.IsStructuralEqual(expected);
-        }
+            ConfigManager.Save(src, this._destPath);
 
-        [TestMethod]
-        public void SerializeCorrectlyObsoleteTest()
-        {
-            var jsonPath = @"TestData\BFCommandConfigJsons\Obsolete.json";
-
-            var expected = new BFCommandConfig
-            {
-                Increment = new BFCommand("inc", BFCommandType.Increment),
-                Decrement = new BFCommand("dec", BFCommandType.Decrement),
-                MoveRight = new BFCommand("moveR", BFCommandType.MoveRight),
-                MoveLeft = new BFCommand("moveL", BFCommandType.MoveLeft),
-                LoopHead = new BFCommand("roopH", BFCommandType.LoopHead),
-                LoopTail = new BFCommand("roopT", BFCommandType.LoopTail),
-                Read = new BFCommand("read", BFCommandType.Read),
-                Write = new BFCommand("write", BFCommandType.Write),
-                BeginComment = new BFCommand("beginC", BFCommandType.BeginComment),
-                EndComment = new BFCommand("endC", BFCommandType.EndComment)
-            };
-
-            var result = ConfigManager.Import<BFCommandConfig>(jsonPath);
-
-            result.IsStructuralEqual(expected);
+            FileAssert.AreEqual(expectedFile, this._destPath);
         }
     }
 }
