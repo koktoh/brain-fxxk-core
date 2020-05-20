a3dcad5c5000b73e85cb1c2cd6fbee840d853660
koktoh <koktoh96@gmail.com>
2020-05-20 21:18:32 +0900

    add BFExceptions

---
+++ brain-fxxk-core/Exception/BFException.cs
@@ -0,0 +1,9 @@
+﻿namespace BFCore.Exception
+{
+    public class BFException : System.Exception
+    {
+        public BFException() : base() { }
+        public BFException(string message) : base(message) { }
+        public BFException(string message, System.Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ brain-fxxk-core/Exception/BFRuntimeException.cs
@@ -0,0 +1,9 @@
+﻿namespace BFCore.Exception
+{
+    public class BFRuntimeException : BFException
+    {
+        public BFRuntimeException() : base() { }
+        public BFRuntimeException(string message) : base(message) { }
+        public BFRuntimeException(string message, System.Exception innerException) : base(message, innerException) { }
+    }
+}
